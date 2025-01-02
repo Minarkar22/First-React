a18344aac2988cca1994785a693ed5ea51a60447
minarkar22 <arkarminnaing98@gmail.com>
2025-01-02 15:07:01 +0300

    event handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,73 @@
 import * as React from 'react';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-const List = () => {
+const Button = () => {
+  const clickHandler = () => {
+    console.log ("clicked on Button");
+  }
   return (
-    <ul>
-      {list.map((item) => {
-        return (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span><br />
-            <span>{item.author}</span><br />
-            <span>{item.num_comments}</span><br />
-            <span>{item.points}</span>
-          </li>
-        );
-      })}
-    </ul>
+    <div>
+      <button type='button' onClick={clickHandler} className='border-2 border-black hover:bg-gray-800'>
+        Click
+      </button>
+    </div>
   )
 }
 
-const Search = () => {
-  const handleChange = (event) => {
-      console.log(event.target.value);
-  };
+const Countfun = () => {
+  const [count, setCount] = React.useState(0);
+  function countPlusHandle () {
+    setCount(count + 1);
+  }
+
+  function countMinusHandle () {
+    setCount(count - 1);
+  }
+
+  function countReset () {
+    setCount (0);
+  }
   return (
     <div>
-      <label htmlFor="search">Search</label>
-      <input type="text" id='search' onChange={handleChange}/>
+      Count : {count}
+      <button type='button' onClick={countPlusHandle} className='border-2 border-black rounded-lg m-3'>
+        Increase count
+      </button>
+      <button type='button' onClick={countMinusHandle} className='border-2 border-black rounded-lg m-3'>
+        Descrese count
+      </button>
+      <button type='button' onClick={countReset} className='border-2 border-black rounded-lg m-3'>
+        Reset count
+      </button>
     </div>
   );
 }
 
-function App() {
+const ChangeText = () => {
+  const [text, setText] = React.useState("");
+  function handleChange(event) {
+    setText(event.target.value);
+  }
   return (
     <div>
-      <h1>Hello My hackear stories</h1>
-      <Search/>
-      <List/>
+      <h3>Input field using event</h3>
+      <input type="text" value={text} onChange={handleChange} className='border-2 border-black rounded-md'/><br />
+      Text: {text}
+    </div>
+  );
+}
+
+
+
+function App() {
+  return (
+    <div className='p-[20px]'>
+      <div className='p-4 bg-blue-500 text-white mb-[50px]'>
+        <h1 className='text-2xl font-bold'>Hello My hackear stories</h1>
+      </div>
+      <Button/>
+      <Countfun/>
+      <ChangeText/>
+      
     </div>
   
   );
